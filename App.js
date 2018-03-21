0ab4faf51a05ba50471cbf736b6ab34e9807e455
Edward Nardo <35951824+asfopoo@users.noreply.github.com>
2018-03-21 00:55:35 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,203 @@
+import React from 'react';
+import { Button, View, Text, StyleSheet, Image, ListView, TextInput, ActivityIndicator, Alert } from 'react-native';
+import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
+
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Main Page</Text>
+         <Image
+          style={styles.stretch}
+          source={require('./keokikid.jpeg')}
+        />
+         <Button
+          title="Search Our Music"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <Button
+          title="Check Out Our Schedule"
+          onPress={() => this.props.navigation.navigate('Schedule')}
+        />
+      </View>
+    );
+  }
+}
+
+class ScheduleScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         
+        <Text> Our Schedule </Text>
+        <Text> Blah Blah </Text>
+        <Text> Blah </Text>
+      </View>
+    );
+  }
+}
+ 
+
+
+class DetailsScreen extends React.Component {
+  constructor(props) {
+ 
+    super(props);
+ 
+    this.state = {
+ 
+      isLoading: true,
+      text: '',
+    
+    }
+ 
+    this.arrayholder = [] ;
+  }
+ 
+  componentDidMount() {
+ 
+    return fetch('https://reactnativecode.000webhostapp.com/FruitsList.php')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
+ 
+          // In this block you can do something with new state.
+          this.arrayholder = responseJson ;
+ 
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      
+  }
+ 
+  GetListViewItem (fruit_name) {
+    
+   Alert.alert(fruit_name);
+  
+  }
+  
+   SearchFilterFunction(text){
+     
+     const newData = this.arrayholder.filter(function(item){
+         const itemData = item.fruit_name.toUpperCase()
+         const textData = text.toUpperCase()
+         return itemData.indexOf(textData) > -1
+     })
+     this.setState({
+         dataSource: this.state.dataSource.cloneWithRows(newData),
+         text: text
+     })
+ }
+ 
+  ListViewItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: .5,
+          width: "100%",
+          backgroundColor: "#000",
+        }}
+      />
+    );
+  }
+ 
+ 
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+ 
+    return (
+ 
+      <View style={styles.MainContainer}>
+ 
+      <TextInput 
+       style={styles.TextInputStyleClass}
+       onChangeText={(text) => this.SearchFilterFunction(text)}
+       value={this.state.text}
+       underlineColorAndroid='transparent'
+       placeholder="Search Here"
+        />
+ 
+        <ListView
+ 
+          dataSource={this.state.dataSource}
+ 
+          renderSeparator= {this.ListViewItemSeparator}
+ 
+          renderRow={(rowData) => <Text style={styles.rowViewContainer} 
+ 
+          onPress={this.GetListViewItem.bind(this, rowData.fruit_name)} >{rowData.fruit_name}</Text>}
+ 
+          enableEmptySections={true}
+ 
+          style={{marginTop: 10}}
+ 
+        />
+ 
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+    Schedule: {
+      screen: ScheduleScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+const styles = StyleSheet.create({
+ 
+ MainContainer :{
+ 
+  justifyContent: 'center',
+  flex:1,
+  margin: 7,
+ 
+  },
+ 
+ rowViewContainer: {
+   fontSize: 17,
+   padding: 10
+  },
+ 
+  TextInputStyleClass:{
+        
+   textAlign: 'center',
+   height: 40,
+   borderWidth: 1,
+   borderColor: '#009688',
+   borderRadius: 7 ,
+   backgroundColor : "#FFFFFF"
+        
+   }
+ 
+});
