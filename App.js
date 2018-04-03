c059859666db5ca42c6010d0e3d13f04529540b4
Edward Nardo <35951824+asfopoo@users.noreply.github.com>
2018-04-03 18:30:01 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import { Button, View, Text, StyleSheet, Image, ListView, TextInput, ActivityIndicator, Alert } from 'react-native';
+import React, { Component } from 'react';
+import { Button, View, ScrollView, Linking, ImageBackground, TouchableOpacity, Text, StyleSheet, Image, ListView, TextInput, ActivityIndicator, Alert } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
+
+
 
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Main Page</Text>
-         <Image
-          style={styles.stretch}
-          source={require('./keokikid.jpeg')}
-        />
-         <Button
-          title="Search Our Music"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-        <Button
-          title="Check Out Our Schedule"
+      <View style={styles.mainStyle}>{/*main page*/}
+        
+          <Image
+            style={styles.stretch}
+            source={require('./bkeoki.png')}
+          /> 
+         
+
+         <TouchableOpacity
+         style={styles.button}
+         onPress={() => this.props.navigation.navigate('Search')}
+          >
+         <Text style= {styles.text}>Search our music</Text>
+         </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
           onPress={() => this.props.navigation.navigate('Schedule')}
-        />
+        >
+         <Text style= {styles.text}>Check Out Our Schedule</Text>
+         </TouchableOpacity>
+
+         <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('WebSearch')}
+        >
+         <Text style= {styles.text}>Cant find a song?</Text>
+         </TouchableOpacity>
+
+         <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('Contact')}
+        >
+         <Text style= {styles.text}>Contact us</Text>
+         </TouchableOpacity>
+
+          <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('Donate')}
+        >
+         <Text style= {styles.text}>Donate</Text>
+         </TouchableOpacity>
+
       </View>
     );
   }
@@ -28,11 +59,111 @@ class HomeScreen extends React.Component {
 class ScheduleScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+       
+       <View style={styles.mainStyle}>
+
+        <ImageBackground source={require('./back.jpg')} style={styles.backgroundImage} > 
          
-        <Text> Our Schedule </Text>
-        <Text> Blah Blah </Text>
-        <Text> Blah </Text>
+          <Text style={styles.bigText}> Our Schedule </Text>{/*Schedule screen*/}
+          <Text style={styles.midText}> All show times are from 9pm-1am </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text style={styles.blackText}> Tuesday: Granfalloons </Text>
+          <Text style={styles.blackText}> Wednesday: Waterway</Text>
+          <Text style={styles.blackText}> Thursday: The cove </Text>
+          <Text style={styles.blackText}> Friday: Primanti Bros York</Text>
+          <Text style={styles.blackText}> Saturday: Primanti Bros Lancaster</Text>
+          <Text style={styles.blackText}> Sunday: Granfalloons </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text style={styles.blackText}> Want Karaoke Kid to do you next event or wedding?</Text>
+          <Text style={styles.blackText}> Just contact us</Text>
+          <Text style={styles.blackText}> Wed be happy to talk about it!!</Text>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+class WebSearchScreen extends React.Component {
+  render() {
+    return (
+       
+      <View style={styles.mainStyle}>
+
+           <Image
+            style={styles.stretch}
+            source={require('./orkeokikid.jpg')}
+          />  
+
+          <Text> </Text>
+          <Text style={styles.text}> Can't find what you're looking for? </Text>
+          <Text> </Text>
+          <Text style={styles.text}> Go to this website, find a song,</Text>
+          <Text style={styles.text}> and we'll buy it for you</Text>
+          <Text> </Text>
+          <Text style={styles.text}
+            onPress={() => Linking.openURL('http://www.karaoke-version.com/karaoke/')}>
+            --CLICK HERE--
+           </Text> 
+        
+       
+      </View>
+    );
+  }
+}
+
+class ContactScreen extends React.Component {
+  render() {
+    return (
+       
+      <View style={styles.mainStyle}> 
+
+          <Image
+            style={styles.stretch}
+            source={require('./keokikid.jpeg')}
+          /> 
+
+          <Text style={styles.text}> Wanna get in touch with the Karaoke Kid </Text>
+          <Text> </Text>
+          <Text style={styles.text}> Find us on Facebook </Text>
+          <Text style={styles.text}
+            onPress={() => Linking.openURL('https://www.facebook.com/The-Karaoke-Kid-York-Pa-278396102214309/')}>
+            Facebook/Karaoke-Kid-York-Pa
+           </Text>
+           <Text> </Text>
+          <Text style={styles.text}>Email us</Text>
+          <Text style={styles.text}>thekaraokekid@yahoo.com</Text>
+          <Text>  </Text>
+          <Text style={styles.text}> Or check out our schedule and come find us!!!!</Text>
+        
+       
+      </View>
+    );
+  }
+}
+
+class DonateScreen extends React.Component {
+  render() {
+    return (
+      
+
+      <View style={styles.mainStyle}> 
+        <ImageBackground source={require('./bkbg.jpg')} style={styles.backgroundImage} > 
+          <Text> </Text>
+          <Text style={styles.bigTextWhite}> Donate to us </Text>
+          <Text style={styles.midTextWhite}> If youd like to contribute since we bought you a song or youd just like to donate </Text>
+          <Text> </Text>
+          <Text style={styles.midTextWhite}
+            onPress={() => Linking.openURL('https://www.patreon.com/thekaraokekid')}>
+            --Click here--
+           </Text>
+          <Text> </Text>
+          <Text style={styles.midTextWhite}> We thank you!!!!!!! </Text>
+          <Text style={styles.midTextWhite}> Hadoken </Text>
+        </ImageBackground>
+       
       </View>
     );
   }
@@ -40,10 +171,10 @@ class ScheduleScreen extends React.Component {
  
 
 
-class DetailsScreen extends React.Component {
+class SearchScreen extends React.Component {
   constructor(props) {
  
-    super(props);
+    super(props);{/*search screen*/}
  
     this.state = {
  
@@ -57,8 +188,8 @@ class DetailsScreen extends React.Component {
  
   componentDidMount() {
  
-    return fetch('https://reactnativecode.000webhostapp.com/FruitsList.php')
-      .then((response) => response.json())
+    return fetch('https://raw.githubusercontent.com/asfopoo/Kareoke-Kid-/master/list.txt') 
+      .then((response) => response.json()) 
       .then((responseJson) => {
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.setState({
@@ -66,7 +197,7 @@ class DetailsScreen extends React.Component {
           dataSource: ds.cloneWithRows(responseJson),
         }, function() {
  
-          // In this block you can do something with new state.
+        
           this.arrayholder = responseJson ;
  
         });
@@ -77,16 +208,16 @@ class DetailsScreen extends React.Component {
       
   }
  
-  GetListViewItem (fruit_name) {
+  GetListViewItem (song) {
+   Alert.alert(song); ///pulls up song when clicked*/}
     
-   Alert.alert(fruit_name);
   
   }
   
    SearchFilterFunction(text){
      
-     const newData = this.arrayholder.filter(function(item){
-         const itemData = item.fruit_name.toUpperCase()
+     const newData = this.arrayholder.filter(function(item){ {/*changed list so it can be mainipulated by react*/}
+         const itemData = item.song.toUpperCase()
          const textData = text.toUpperCase()
          return itemData.indexOf(textData) > -1
      })
@@ -96,11 +227,11 @@ class DetailsScreen extends React.Component {
      })
  }
  
-  ListViewItemSeparator = () => {
+  ListViewItemSeparator = () => { {/*sets list seperator and style*/}//line seperator b/w songs in list
     return (
       <View
         style={{
-          height: .5,
+          height: 0,
           width: "100%",
           backgroundColor: "#000",
         }}
@@ -109,28 +240,28 @@ class DetailsScreen extends React.Component {
   }
  
  
-  render() {
+  render() { {/*if the page takes time to load*/}
     if (this.state.isLoading) {
       return (
         <View style={{flex: 1, paddingTop: 20}}>
           <ActivityIndicator />
-        </View>
+        </View> 
       );
     }
  
     return (
  
       <View style={styles.MainContainer}>
- 
+         {/*input to search list*/} 
       <TextInput 
        style={styles.TextInputStyleClass}
        onChangeText={(text) => this.SearchFilterFunction(text)}
        value={this.state.text}
        underlineColorAndroid='transparent'
-       placeholder="Search Here"
+       placeholder="Search by Artist or song title"
         />
- 
-        <ListView
+          {/*how list is displayed*/}
+        <ListView 
  
           dataSource={this.state.dataSource}
  
@@ -138,7 +269,7 @@ class DetailsScreen extends React.Component {
  
           renderRow={(rowData) => <Text style={styles.rowViewContainer} 
  
-          onPress={this.GetListViewItem.bind(this, rowData.fruit_name)} >{rowData.fruit_name}</Text>}
+          onPress={this.GetListViewItem.bind(this, rowData.song)} >{rowData.song}</Text>}
  
           enableEmptySections={true}
  
@@ -150,38 +281,47 @@ class DetailsScreen extends React.Component {
     );
   }
 }
-
-const RootStack = StackNavigator(
+{/*declares screens to set up navigation*/}
+const RootStack = StackNavigator( 
   {
-    Home: {
+    Home: { 
       screen: HomeScreen,
     },
-    Details: {
-      screen: DetailsScreen,
+    Search: {
+      screen: SearchScreen,
     },
     Schedule: {
       screen: ScheduleScreen,
+    },
+    WebSearch: {
+      screen: WebSearchScreen,
+    },
+    Contact: {
+      screen: ContactScreen,
+    },
+    Donate: {
+      screen: DonateScreen,
     },
   },
   {
     initialRouteName: 'Home',
   }
 );
+ {/*tells react which page to start at*/}
 
 export default class App extends React.Component {
   render() {
     return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
+{/*styling*/}
+const styles = StyleSheet.create({ 
  
  MainContainer :{
  
   justifyContent: 'center',
   flex:1,
   margin: 7,
- 
   },
  
  rowViewContainer: {
@@ -194,10 +334,81 @@ const styles = StyleSheet.create({
    textAlign: 'center',
    height: 40,
    borderWidth: 1,
-   borderColor: '#009688',
+   borderColor: '#ff0000',
    borderRadius: 7 ,
    backgroundColor : "#FFFFFF"
         
-   }
+   },
+
+  mainStyle: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+    backgroundColor: '#3d3d3d',
+  
+  
+   },
+
+    backgroundImage: {
+      flex: 1,
+      alignSelf:'stretch',
+      alignItems: 'center',
+      width: null,
+      justifyContent: 'center',
+
+     
+  },
+
+   button: {
+   backgroundColor: '#3d3d3d', //color of button 
+    borderRadius: 10,  //changes the rounded edges
+    padding: 10, //changes height/width around text of button
+    marginBottom: 5, //marin under button
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 }, // shadow stylings unessary since opacity is set to 0
+    shadowRadius: 10,
+    shadowOpacity: 0, // makes shadow invisible.. mainly affects iphone not androic
+  },
+
+  text: {
+        textAlign: 'center',
+        color: 'white',
+        
+    },
+
+   bigText: {
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 32
+        
+    },   
+
+    midText: {
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 20
+        
+    },  
+
+    blackText: {
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 20
+    },  
+
+    bigTextWhite: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 32
+        
+    },   
+
+    midTextWhite: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20
+        
+    },  
+
  
 });
